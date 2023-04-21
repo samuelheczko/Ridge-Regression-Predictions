062a51ab235d77ff6bfb6e5d3c3dd34f4aceb946
Samuel Heczko <samuelheczko@gmail.com>
2023-04-21 15:34:36 +0200

    fist changed

--- fist.sh
+++ fist.sh
@@ -5,18 +5,14 @@
 #SBATCH --exclusive
 #SBATCH --time=0-24:00:00
 #SBATCH --partition compute
+#SBATCH --output=/home/sheczko/logs/%j.out
+
  
 module add singularity
-export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
- 
-SCRATCH_DIRECTORY=/ptmp/${USER}/${SLURM_JOBID}
-mkdir -p ${SCRATCH_DIRECTORY}
-cd ${SCRATCH_DIRECTORY}
- 
-cp ${SLURM_SUBMIT_DIR}/.py ${SCRATCH_DIRECTORY}
 
+export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
 
 singularity exec /ptmp/containers/fmriprep_latest.sif  python /home/sheczko/code1/MastersThesis/FS_MS.py
-cp -r ${SCRATCH_DIRECTORY}/output ${SLURM_SUBMIT_DIR}/${SLURM_JOBID}
- 
+
+
 exit 0
