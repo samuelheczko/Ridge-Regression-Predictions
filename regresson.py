5062842423802a822c15badcc7ab33ad5593dd95
Samuel Heczko <samuelheczko@gmail.com>
2023-05-04 20:12:59 +0200

    added regression

--- regresson.py
+++ regresson.py
@@ -3,6 +3,27 @@
 import pandas as pd
 import numpy as np
 import glob
+#import relevant libraries
+import sys; sys.path
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+from sklearn.metrics import explained_variance_score, r2_score
+from sklearn.linear_model import Ridge
+#from sklearn.kernel_ridge import KernelRidge
+#from sklearn.linear_model import Lasso
+#from sklearn.svm import SVR
+#from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.preprocessing import StandardScaler
+#from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.pipeline import Pipeline
+
+
 
 def load_data(path):
     #INPUT: the path to the desired atlas
@@ -34,3 +55,121 @@ def load_regressors(path):
     regressors_df['subj_ids'] = subj_ids
 
     return regressors_df  #subjects as row, regressors as colums (one of them being the subject id)
+
+
+def regression(X, Y, perm, cv_loops, k, train_size, n_cog, regr, alphas,n_feat,cognition):
+    ##input X, Y, amount of permutations done on the cv, k: amont of inner loops ot find optimal alpha, train_size: the proption of training dataset, n_cog: the amount of behavioural vairables tested, model:regression type, alhaps_n; the range of alphas to be searched, n_feat: the amount of features
+
+    
+
+
+    #create arrays to store variables
+    #r^2 - coefficient of determination
+    r2 = np.zeros([perm,n_cog])
+    #explained variance
+    var = np.zeros([perm,n_cog])
+    #correlation between true and predicted (aka prediction accuracy)
+    corr = np.zeros([perm,n_cog])
+    #optimised alpha (hyperparameter)
+    opt_alpha = np.zeros([perm,n_cog])
+    #predictions made by the model
+    preds = np.zeros([perm,n_cog,int(np.ceil(X.shape[0]*(1-train_size)))])
+    #true test values for cognition
+    cogtest = np.zeros([perm,n_cog,int(np.ceil(X.shape[0]*(1-train_size)))])
+    #feature importance extracted from the model
+    featimp = np.zeros([perm,n_feat,n_cog])
+
+    #set the param grid be to the hyperparamters you want to search through
+    paramGrid ={'alpha': alphas}
+
+    #iterate through permutations
+    for p in range(perm):
+        #print permutation # you're on
+        print('Permutation %d' %(p+1))
+        #split data into train and test sets
+        x_train, x_test, cog_train, cog_test = train_test_split(X, Y, test_size=1-train_size, 
+                                                                shuffle=True, random_state=p)
+
+        
+        #iterate through the cognitive metrics you want to predict
+        for cog in range (n_cog):
+
+            #print cognitive metrics being predicted 
+            print ("Cognition: %s" % cognition[cog])
+            
+            #set y values for train and test based on     
+            y_train = cog_train[:,cog]
+            y_test = cog_test[:,cog]
+            
+            #store all the y_test values in a separate variable that can be accessed later if needed
+            cogtest[p,cog,:] = y_test
+
+
+            #create variables to store nested CV scores, and best parameters from hyperparameter optimisation
+            nested_scores = []
+            best_params = []
+            
+
+            #optimise regression model using nested CV
+            print('Training Models')
+            
+            #go through the loops of the cross validation
+            for i in range(cv_loops):
+
+
+                #set parameters for inner and outer loops for CV
+                inner_cv = KFold(n_splits=k, shuffle=True, random_state=i)
+                outer_cv = KFold(n_splits=k, shuffle=True, random_state=i)
+                
+                #define regressor with grid-search CV for inner loop
+                gridSearch = GridSearchCV(estimator=regr, param_grid=paramGrid, n_jobs=-1, 
+                                        verbose=0, cv=inner_cv, scoring='r2')
+
+                #fit regressor
+                gridSearch.fit(x_train, y_train)
+
+                #save parameters corresponding to the best score
+                best_params.append(list(gridSearch.best_params_.values()))
+
+                #call cross_val_score for outer loop
+                nested_score = cross_val_score(gridSearch, X=x_train, y=y_train, cv=outer_cv, 
+                                            scoring='r2', verbose=1)
+
+                #record nested CV scores
+                nested_scores.append(np.median(nested_score))
+
+                #print how many cv loops are complete
+                print("%d/%d Complete" % (i+1,cv_loops))
+                
+            #once all CV loops are complete, fit models based on optimised hyperparameters    
+            print('Testing Models')
+
+
+            #save optimised alpha values
+            opt_alpha[p,cog] = np.median(best_params)
+
+
+            #fit model using optimised hyperparameter
+            model = Ridge(fit_intercept = True, alpha = opt_alpha[p,cog], max_iter=1000000)
+
+            model.fit(x_train, y_train)
+            
+            #compute r^2 (coefficient of determination) 
+            r2[p,cog]=model.score(x_test,y_test)
+
+            #generate predictions from model
+            preds[p,cog,:] = model.predict(x_test).ravel()
+            
+            #compute explained variance 
+            var[p,cog] = explained_variance_score(y_test, preds[p,cog,:])
+
+            #compute correlation between true and predicted
+            corr[p,cog] = np.corrcoef(y_test, preds[p,cog,:])[1,0]
+
+            #extract feature importance
+            featimp[p,:,cog] = model.coef_
+        
+    return r2, preds, var, corr, featimp, cogtest,opt_alpha
+
+
+
---
+++ regresson_launcher.py
@@ -0,0 +1,92 @@
+
+import glob
+import os
+import connectome
+import nibabel as nib
+import pandas as pd
+import regresson
+
+import sys; sys.path
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+from sklearn.metrics import explained_variance_score, r2_score
+from sklearn.linear_model import Ridge
+
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
+
+cluster = False
+if cluster:
+    path = '/home/sheczko/ptmp/data/' #global path for cluster
+else:
+    path = 'data/' ##local path for local computations
+
+
+
+
+##let's find the paths to the the csv connectivity data we ahve
+data_paths = glob.glob(path + '/results/connectomes/*.csv')
+
+##load up regresors
+regressors_df =  regresson.load_regressors(path + 'func_images/AOMIC/regressors/*.txt')
+##choose the target variables, take as np arrays
+GCA = regressors_df.regressor_iq.values
+bmi = regressors_df.regressor_bmi.values
+
+ #concatenate cognitive metrics into single variable
+cognition = ['GCA','bmi']
+#cognition = ['PMAT Correct', 'PMAT Response Time']
+cog_metric = np.transpose(np.asarray([GCA, bmi]))
+
+#set the number of permutations you want to perform
+perm = 100
+    #set the number of cross-validation loops you want to perform
+cv_loops = 5
+#set the number of folds you want in the inner and outer folds of the nested cross-validation
+k = 3
+#set the proportion of data you want in your training set
+train_size = .8
+#set the number of variable you want to predict to be the number of variables stored in the cognition variablse
+n_cog = np.size(cognition)
+#set regression model type
+regr = Ridge(fit_intercept = True, max_iter=1000000)
+#set hyperparameter grid space you want to search through for the model
+alphas = np.linspace(10, 10000, num = 100, endpoint=True, dtype=None, axis=0)
+#set y to be the cognitive metrics you want to predict. They are the same for every atlas (each subject has behavioural score regradless of parcellation)
+Y = cog_metric
+
+
+for data_path_i, data_path in enumerate(data_paths): ##loop over atlases
+    
+    cuurent_path = data_paths[data_path_i+1]
+    current_atlas = cuurent_path.split('/')[-1].split('-')[-1].split('.')[0]
+    print(f'current atlas: ' + current_atlas)
+
+    fc = regresson.load_data(cuurent_path) ##set the imput variable to the current atlas connectome, gives subjects x features matrix
+
+    #set x data to be the input variable you want to use (we use always fc)
+    
+    X = fc
+    X[X<0] = 0 #filter the negative values from the correlations
+
+
+    #set the number of features 
+    n_feat = X.shape[1]
+    r2, preds, var, corr, featimp, cogtest,opt_alphas = regresson.regression(X = X, Y = Y, perm = perm, cv_loops = cv_loops, k = k, train_size = 0.8, n_cog = n_cog, regr = regr, alphas = alphas,n_feat = n_feat,cognition = cognition)
+    
+    ##save data:
+    result_df = pd.DataFrame() ## empty dataframe, filled with colums where each row is one permutation
+    preds_df = pd.DataFrame()
+
+    for cog_i, cog_names in enumerate(cognition):
+            result_df[f'{cog_names}' + '_r2'] = r2[:,cog_i] #fill in the r2 values
+            result_df[f'{cog_names}' + '_var'] = var[:,cog_i] #fill in explained variation values
+            result_df[f'{cog_names}' + '_opt_alphas'] = opt_alphas[:,cog_i] #fill in optimal alpha values
+            for perm_n in range(preds.shape[0]): 
+                preds_df[f'{cog_names}_perm_{perm_n + 1}_preds'] = preds[perm_n,cog_i,:] #fill in preditions
+                preds_df[f'{cog_names}_perm_{perm_n + 1}_real'] = cogtest[perm_n,cog_i,:] #fill in real_values
+    result_df.to_csv(path + f'results/ridge_regression/ridge_results_atlas-{current_atlas}.csv')
+    preds_df.to_csv(path + f'results/ridge_regression/ridge_preds_atlas-{current_atlas}.csv')
