17ea0df10e6200353f1e5607a2fe9177dd48b995
Samuel Heczko <samuelheczko@gmail.com>
2023-05-15 16:44:30 +0200

    wrong shell launher script

--- shell_ICA_connectomes_launcher.sh
+++ shell_ICA_connectomes_launcher.sh
@@ -12,7 +12,7 @@ module add singularity
 
 export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
 
-singularity exec /ptmp/containers/datascience-python_latest.sif  python /home/sheczko/code1/MastersThesis/connectome_extraction_megaloop.py
+singularity exec /ptmp/containers/datascience-python_latest.sif  python /home/sheczko/code1/MastersThesis/connectome_extraction_megaloop_ICA.py
 
 
 exit 0
