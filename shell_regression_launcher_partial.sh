219bf8fabecc928811353c22f923ea83837d78db
Samuel Heczko <samuelheczko@gmail.com>
2023-05-16 17:29:01 +0200

    added all correaltions and regressions

--- shell_regression_launcher_partial.sh
+++ shell_regression_launcher_partial.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
-#SBATCH --job-name=connectome_extraction
+#SBATCH --job-name=regresson_partial
+
  
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --time=0-24:00:00
+#SBATCH --time=0-120:00:00
 #SBATCH --partition compute
 #SBATCH --output=/home/sheczko/logs/%j.out
 
@@ -12,7 +13,7 @@ module add singularity
 
 export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
 
-singularity exec /ptmp/containers/datascience-python_latest.sif  python /home/sheczko/code1/MastersThesis/regresson_launcher_tangent.py
+singularity exec /ptmp/containers/datascience-python_latest.sif  python /home/sheczko/code1/MastersThesis/regresson_launcher_partial.py
 
 
 exit 0
---
+++ shell_regression_launcher_precision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=regresson_precision
+
+ 
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=0-120:00:00
+#SBATCH --partition compute
+#SBATCH --output=/home/sheczko/logs/%j.out
+
+ 
+module add singularity
+
+export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
+
+singularity exec /ptmp/containers/datascience-python_latest.sif  python /home/sheczko/code1/MastersThesis/regresson_launcher_precision.py
+
+
+exit 0
