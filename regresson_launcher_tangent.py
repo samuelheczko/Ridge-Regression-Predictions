ee5ee559b8393251a01966cbb617d46e503180ec
Samuel Heczko <samuelheczko@gmail.com>
2023-06-01 15:49:46 +0200

    new optimisation parameters

--- regresson_launcher_tangent.py
+++ regresson_launcher_tangent.py
@@ -117,7 +117,7 @@ for data_path_i, data_path in enumerate(csv_paths): ##loop over atlases
     #set hyperparameter grid space you want to search through for the model
     #alphas = np.linspace(max(n_feat*0.12 - 1000, 0.0001), n_feat*0.12 + 2000, num = 50, endpoint=True, dtype=None, axis=0) #set the range of alpahs being searhced based off the the amount of features
     alphas = loguniform(10, 10e4)
-    n_iter = 20
+    n_iter = 100
 
 
 
