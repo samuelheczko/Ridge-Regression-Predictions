41df76725889f6efe04b863f98ecaa09e74968a8
Samuel Heczko <samuelheczko@gmail.com>
2023-06-20 11:42:18 +0200

    SVR launcher

---
+++ shell_SVR_launcher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --job-name=connectome_extraction
+ 
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=0-24:00:00
+#SBATCH --partition compute
+#SBATCH --output=/home/sheczko/logs/%j.out
+
+ 
+module add singularity
+
+export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
+
+singularity exec /ptmp/containers/datascience-python_latest.sif  python /home/sheczko/code1/MastersThesis/SVR_launcher_tangent.py
+
+
+exit 0
